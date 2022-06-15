04cb8c72ca3993f6d555425f32277e2d4a8968f0
jaksa97 <veljko.jaksic@pmf.edu.rs>
2022-06-16 01:18:04 +0200

    Initial commit

---
+++ src/main/java/com/github/jaksa97/LeafSaver/LeafSaverApplication.java
@@ -0,0 +1,12 @@
+package com.github.jaksa97.LeafSaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeafSaverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeafSaverApplication.class, args);
+    }
+}
